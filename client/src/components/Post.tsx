0712d7b5c2bebb81967d5bb764cadd27d7ccc571
Noam Ifat <noamifat@gmail.com>
2025-01-07 18:34:31 +0200

    add profile page

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React from "react";
 
 interface PostProps {
-    username: string;
-    userPhoto: string;
-    postPhoto: string;
-    description: string;
+  username: string;
+  userPhoto: string;
+  postPhoto: string;
+  description: string;
 }
 
-const Post: React.FC<PostProps> = ({ username, userPhoto, postPhoto, description }) => {
-    return (
-        <div className="post card mb-3">
-            <div className="card-body">
-                <div className="d-flex align-items-center mb-3">
-                    <img src={userPhoto} alt={username} className="rounded-circle user-photo m-2" style={{ width: "30px", height: "30px" }}/>
-                    <span className="ml-3">{username}</span>
-                </div>
-                <img src={postPhoto} alt="Post" className="post-photo mb-3" style={{ width: "60vw", height: "30vh" }}/>
-                <p>{description}</p>
-            </div>
+const Post: React.FC<PostProps> = ({
+  username,
+  userPhoto,
+  postPhoto,
+  description,
+}) => {
+  return (
+    <div className="post card mb-3" style={{ width: "40vw", height: "40vh" }}>
+      <div className="card-body">
+        <div className="d-flex align-items-center mb-3">
+          <img
+            src={userPhoto}
+            alt={username}
+            className="rounded-circle user-photo m-2"
+            style={{ width: "30px", height: "30px" }}
+          />
+          <span className="ml-3">{username}</span>
         </div>
-    );
+        <img
+          src={postPhoto}
+          alt="Post"
+          className="post-photo mb-3"
+          style={{ width: "30vw", height: "20vh" }}
+        />
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
