b58b9ed0d5b06d786d92b267eb5a0909c1e03a29
Noam Ifat <noamifat@gmail.com>
2025-01-07 19:59:15 +0200

    user data

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import postsData from "../data/posts.json";
 import Post from "../components/Post";
+import UserProfile from "../components/UserProfile";
 
 const Profile: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 2;
   const currentUsername = "PlantLover";
   const [posts, setPosts] = useState<Post[]>([]);
+  const [userProfile, setUserProfile] = useState({
+    username: "PlantLover",
+    email: "plantlover@example.com",
+    profilePhoto: "/path/to/profile-photo.jpg", // Replace with actual image path or state
+  });
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
@@ -22,7 +28,7 @@ const Profile: React.FC = () => {
         currentPage * postsPerPage
       )
     );
-  }, []);
+  }, [currentPage]);
 
   const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
 
@@ -37,58 +43,83 @@ const Profile: React.FC = () => {
   const deletePost = (post: Post) => {
     // TODO post delete
   };
+
+  const handleSaveProfile = (
+    updatedUsername: string,
+    updatedProfilePhoto: string | null
+  ) => {
+    setUserProfile((prevProfile) => ({
+      ...prevProfile,
+      username: updatedUsername,
+      profilePhoto: updatedProfilePhoto || prevProfile.profilePhoto,
+    }));
+  };
   return (
     <>
-      {
-        <div className="container mt-4 d-flex flex-column justify-content-center">
-          {/* Posts Section */}
-          <div
-            className="row flex-grow-1"
-            style={{ minHeight: "300px", height: "600px" }}
-          >
-            {posts.length > 0 ? (
-              posts.map((post, index) => (
-                <div className="col-12 mb-3" key={index}>
-                  <Post
-                    deletePost={() => deletePost(post)}
-                    editPost={() => editPost(post)}
-                    onEditSave={(updatedPost: Post) => console.log(updatedPost)}
-                    key={post.id}
-                    post={post}
-                  />
-                </div>
-              ))
-            ) : (
-              <div className="col-12 d-flex justify-content-center align-items-center">
-                <p className="text-center">No posts available.</p>
-              </div>
-            )}
-          </div>
-
-          {/* Pagination Section */}
-          <div className="row">
-            <div className="col-12 text-center">
-              {totalPages > 0 ? (
-                [...Array(totalPages).keys()].map((num) => (
-                  <button
-                    key={num}
-                    className={`btn btn-${
-                      num + 1 === currentPage ? "success" : "outline-success"
-                    } mx-1`}
-                    onClick={() => handlePageChange(num + 1)}
-                  >
-                    {num + 1}
-                  </button>
+      <div className="container">
+        <div className="row">
+          <div className="mt-4 d-flex flex-column justify-content-center col-6">
+            {/* Posts Section */}
+            <div
+              className="row flex-grow-1"
+              style={{ minHeight: "300px", height: "600px" }}
+            >
+              {posts.length > 0 ? (
+                posts.map((post, index) => (
+                  <div className="col-12 mb-3" key={index}>
+                    <Post
+                      deletePost={() => deletePost(post)}
+                      editPost={() => editPost(post)}
+                      onEditSave={(updatedPost: Post) =>
+                        console.log(updatedPost)
+                      }
+                      key={post.id}
+                      post={post}
+                    />
+                  </div>
                 ))
               ) : (
-                <button className="btn btn-outline-secondary" disabled>
-                  No Pages
-                </button>
+                <div className="col-12 d-flex justify-content-center align-items-center">
+                  <p className="text-center">No posts available.</p>
+                </div>
               )}
             </div>
+
+            {/* Pagination Section */}
+            <div className="row">
+              <div className="col-12 text-center">
+                {totalPages > 0 ? (
+                  [...Array(totalPages).keys()].map((num) => (
+                    <button
+                      key={num}
+                      className={`btn btn-${
+                        num + 1 === currentPage ? "success" : "outline-success"
+                      } mx-1`}
+                      onClick={() => handlePageChange(num + 1)}
+                    >
+                      {num + 1}
+                    </button>
+                  ))
+                ) : (
+                  <button className="btn btn-outline-secondary" disabled>
+                    No Pages
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+
+          {/* User Profile Section */}
+          <div className="col-6 mt-4" style={{ width: "30%" }}>
+            <UserProfile
+              username={userProfile.username}
+              email={userProfile.email}
+              profilePhoto={userProfile.profilePhoto}
+              onSaveProfile={handleSaveProfile}
+            />
           </div>
         </div>
-      }
+      </div>
     </>
   );
 };
